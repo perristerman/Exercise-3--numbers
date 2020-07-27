b21d688cd936c95108cbe4350c012e55cdf211c8
perristerman <perri.sterman@gmail.com>
2020-07-27 17:10:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+playeranswer = input("Pick any number")
+playeranswer = float(playeranswer)
+if playeranswer == 10:
+  print("That is equal to 10!")
+elif playeranswer < 10:
+  print("That number is less than 10")
+else:
+  print("That number is greater than 10")
